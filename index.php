04ffc7f366beadd5f1bbb540051fd919e8011078
AJGeel <geelaj01@gmail.com>
2020-04-07 12:56:21 +0200

    Removed redundancies from sign up flows. Added 'paper plane' logo + animations / microinteractions

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <meta name="color-scheme" content="light dark">
 
     <!-- Links and CSS -->
-    <link rel="shortcut icon" href="/i/favicon-alt.png" type="image/png" id="favicon">
+    <link rel="shortcut icon" href="/i/requestor.svg" type="image/png" id="favicon">
     <link rel="stylesheet" href="/css/reset.min.css"> <!-- Reset browser inconsistencies -->
     <link rel="stylesheet" href="/css/style.css">
 
@@ -242,13 +242,10 @@
 
     <nav>
       <div class="navbar homepage">
-        <!-- <a class="nav-logo" href="/index.php"><img src="https://placehold.it/36x36" alt="Requestor Logo">Requestor</a> -->
-        <a class="nav-logo" href="/index.php"><div class="navbar--logo"></div>Requestor</a>
-        <div class="urlbar">
-          <input type="text" name="url_input"  placeholder="Enter your Figma link here" />
-          <button type="button" name="url_button">Create New Project<span class="chevron-right"></span>&gt;</button>
-        </div>
-        <a href="#!" class="nav-account">My Account</a>
+        <a class="nav-logo" href="/index.php">
+          <!-- <div class="navbar--logo"></div> -->
+          <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg"> <g clip-path="url(#clip0)"> <path d="M38.1119 12.5519C38.2451 11.8806 37.42 11.1548 36.9908 10.8759L29.4077 13.684C20.0599 12.5239 1.37635 10.7296 1.42434 12.8323C1.48433 15.4608 24.8723 34.991 26.868 35.0332C27.6883 35.0506 31.1903 29.2847 32.4293 26.7848C34.3507 27.6021 36.7716 25.9019 37.4209 23.8502C38.0059 22.0018 36.7683 19.7313 36.0522 18.7817C36.6336 16.9911 37.9787 13.2233 38.1119 12.5519Z" fill="#39574B"/> <path d="M2.7377 10.3757C15.95 9.27681 23.5939 9.16632 36.8046 10.7955C37.2077 10.8452 37.4783 11.2398 37.3713 11.6317C35.3463 19.0473 32.7602 24.1096 26.4657 33.5925C26.251 33.916 25.802 33.991 25.4938 33.7549C15.7064 26.2543 10.298 21.5531 1.40652 13.0734C1.17921 12.8566 1.12977 12.5085 1.29128 12.2391L2.21396 10.7001C2.32516 10.5146 2.5222 10.3937 2.7377 10.3757Z" fill="#6AC4A0"/> <path d="M9.76842 13.8971C17.7175 15.2718 26.4171 18.1013 34.5047 19.2154C34.661 19.2369 34.805 19.3116 34.9096 19.4297C36.2229 20.9128 37.1012 22.532 36.2247 24.3865C36.2074 24.4232 36.1853 24.4593 36.1611 24.4919C34.9146 26.1673 34.4162 26.3033 31.474 24.995C25.5044 22.3405 16.1753 18.3705 9.46622 14.7698C9.22858 14.6423 9.15016 14.3415 9.29219 14.1122C9.39263 13.9501 9.58049 13.8646 9.76842 13.8971Z" fill="#517D6C"/> </g> <defs> <clipPath id="clip0"> <rect width="40" height="40" fill="white"/> </clipPath> </defs> </svg>
+          Requestor</a>
         <button class="nav-sign-in" type="button" name="button" onclick="location.href='/app/sign-in.php'">Sign In</button>
       </div>
     </nav>
@@ -301,14 +298,16 @@
       <p>For an early preview of the tool and its pages, please check out the <a href="#sitemap">sitemap</a> below. The links can be clicked to preview functionalities of the tool.</p>
 
       <div class="sitemap" id="sitemap">
-        <p>Sitemap Version 'Pre-Alpha' &mdash; 2020/03/26</p>
+        <p>Sitemap Version 'Pre-Alpha' &mdash; 2020/04/06</p>
         <ul><li><a href="#!">Landing Page</a><span>(Current page. What is Requestor, Why did we create it, How does it work, Sign-up)</span><span>In-progress</span></li></ul>
         <ul>
           <li><a href="/app/">Application</a><span>(Demonstrator part of the project)</span><span>In-progress</span></li>
           <ul><li><a href="/app/">Dashboard</a><span>(Manage active projects. Access/Create/Edit Prototypes.  Requires login to access)</span><span>In-progress</span></li></ul>
           <ul><li><a href="/app/r">Prototype</a><span>(Embedded prototype with side-evaluation form. Requires URL to access)</span><span>V1 MVP 🎉</span></li></ul>
           <ul><li><a href="/app/reports">Reports</a><span>(Report screen of evaluation results. Requires login to access)</span><span>In-progress</span></li></ul>
-          <ul><li><a href="/app/profile/">Profile Page</a><span>(Contains profile page, settings, disable account)</span><span>In-progress</span></li></ul>
+          <ul><li><a href="/app/profile/">Profile Page</a><span>(Profile page, settings, disable account)</span><span>In-progress</span></li></ul>
+          <ul><li><a href="/app/create-account.php">Create Account</a><span>(Form to create account, links to ToC and Privacy Policy)</span><span>In-progress</span></li></ul>
+          <ul><li><a href="/app/sign-in.php">Sign In</a><span>(Form to sign in, link to recover password)</span><span>In-progress</span></li></ul>
         </ul>
         <ul><li><a href="/terms-and-conditions/">Terms and Conditions</a><span>(Legal agreement between user and provider)</span><span>In-progress</span></li></ul>
         <ul><li><a href="/privacy-policy/">Privacy Policy</a><span>(GDPR: explains how we handle privacy)</span><span>V1 MVP 🎉</span></li></ul>
