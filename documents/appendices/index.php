3c30039e3de19210c02b1659e988976cc106581c
AJGeel <geelaj01@gmail.com>
2020-06-11 17:07:40 +0200

    Included report appendix for deliverables

---
+++ documents/appendices/index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <!-- Meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="noindex, nofollow">
+    <title>Requestor &mdash; Appendix</title>
+    <meta name="description" content="Requestor enables digital creators to collaborate on design prototypes, and provide actionable feedback.">
+    <meta name="author" content="Arthur Geel, hello@arthurgeel.com">
+    <meta name="color-scheme" content="light dark">
+
+    <!-- Links and CSS -->
+    <link rel="shortcut icon" href="/i/requestor.svg" type="image/png" id="favicon">
+    <link rel="stylesheet" href="/css/reset.min.css"> <!-- Reset browser inconsistencies -->
+    <link href="https://fonts.googleapis.com/css2?family=Sen:wght@400;700;800&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+
+    <style media="screen">
+      html {
+        background-color: #ecf3f2;
+        font-family: 'Sen', sans-serif;
+      }
+
+      h1 {
+        font-size: 1.7em;
+        margin-bottom: .5em;
+      }
+
+      p {
+        line-height: 1.5;
+      }
+
+      p.smaller {
+        font-size: .95em;
+        opacity: .7;
+        margin-top: 4em;
+        border: 1px solid black;
+        padding: 1em;
+      }
+
+      a {
+        color: #009973;
+      }
+
+      .wrapper {
+        max-width: 500px;
+        margin: 6em auto;
+      }
+
+      em {
+        font-style: italic;
+        opacity: .7;
+      }
+    </style>
+
+  </head>
+  <body>
+
+    <div class="wrapper">
+      <h1>Appendix</h1>
+      <p><a href="https://www.dropbox.com/sh/bw53om0itc4yt25/AAAJm7cXHv5N5-HyUnIQ8Vgza?dl=0" target="_blank">Click here</a> to access the appendix.</p>
+      <p class="smaller">Note: This page ensures that the appendices to the Requestor FMP report may always be retrieved, as static links to online locations may expire. On this page, you will always be able to find a 'live' link to the appendix. <br><br><em>Last updated: 11-06-20</em></p>
+
+    </div>
+
+
+
+  </body>
+</html>
