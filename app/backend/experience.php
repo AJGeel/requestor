de0ffabcce277d099ae3c631c516da5225e4412e
AJGeel <geelaj01@gmail.com>
2020-07-22 12:47:27 +0200

    Update research questionnaire back-end

--- app/backend/experience.php
+++ app/backend/experience.php
@@ -14,9 +14,11 @@ if ($conn->connect_error) {
 // Escape user inputs for security, prevent sql injection
 $user_id = $conn->real_escape_string($_REQUEST['user_id']);
 $experience_level = $conn->real_escape_string($_REQUEST['experience_level']);
+$novelty_familiarity = $conn->real_escape_string($_REQUEST['novelty_familiarity']);
+$novelty_frequency = $conn->real_escape_string($_REQUEST['novelty_frequency']);
 
 // We create a string that holds the (sanitized) input we have for the MySQL server.
-$sql_input = "INSERT INTO experience (user_id, experience_level) VALUES ('$user_id', '$experience_level')";
+$sql_input = "INSERT INTO experience (user_id, experience_level, novelty_familiarity, novelty_frequency) VALUES ('$user_id', '$experience_level', '$novelty_familiarity', '$novelty_frequency')";
 
 // We try the SQL input. If everything is all-right, the user's data is saved in the database.
 if ($conn->query($sql_input) === TRUE) {
@@ -25,9 +27,6 @@ if ($conn->query($sql_input) === TRUE) {
   http_response_code(200);
 
 } else {
-  // However, if it's not, we'll get some error message telling the user why that happened.
-  // This is mainly for debugging, virtually impossible for an end-user to encounter this.
-  // echo "Error: " . $sql_input . "<br>" . $conn->error;
 
   // Communicate 400 status — Bad Request
   http_response_code(400);
